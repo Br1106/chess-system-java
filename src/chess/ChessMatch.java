a88f54854f9893dfcb42db04a448be32084c6481
Bruno Domingos Teixeira <bruninhodt2467@gmail.com>
2022-11-08 20:03:27 -0300

    Board Exception and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,6 +27,8 @@ public class ChessMatch {
 		board.placePeace(new Rook(board, Color.WHITE), new Position(2, 1));
 		board.placePeace(new King(board, Color.BLACK), new Position(0, 4));
 		board.placePeace(new King(board, Color.BLACK), new Position(7, 4));
+		
+		board.placePeace(new King(board, Color.BLACK), new Position(2, 1));
 
 	}
 }

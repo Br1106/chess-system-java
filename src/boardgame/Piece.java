13948a622c1c20389d9154d5bdac00a971998163
Bruno Domingos Teixeira <bruninhodt2467@gmail.com>
2022-11-07 21:46:32 -0300

    Chass layer & printing the Board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,4 +2,16 @@ package boardgame;
 
 public class Piece {
 
+	protected Position position ;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;// não há necessidade
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
 }
